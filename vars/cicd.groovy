d5f26845b292496bfbc7b8310c99815a4bf0b08d
HarikaVottikonda <72537240+HarikaVottikonda@users.noreply.github.com>
2023-05-01 15:13:37 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,20 @@
+
+
+def newGit(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def newMaven()
+{
+  sh "mvn package"
+}
+
+def newDeploy(jobname,ipaddress,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def newTest(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
